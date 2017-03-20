160690b0139a9f0da65e52d8bbaea2b4b7abde0b
xuchao <85825770@qq.com>
2017-03-20 14:39:04 +0800

    update

--- src/Client.php
+++ src/Client.php
@@ -21,6 +21,82 @@
 
 namespace aliyun\live;
 
-class Client {
-    
+class Client
+{
+    public $secretId;
+    public $secretKey;
+
+    public $version = '2016-11-01';
+
+    /**
+     * @var string 时间格式
+     */
+    public $dateTimeFormat = 'Y-m-d\TH:i:s\Z';
+
+    /**
+     * Client constructor.
+     * @param string $secretId
+     * @param string $secretKey
+     */
+    public function __construct($secretId, $secretKey)
+    {
+        $this->secretId = $secretId;
+        $this->secretKey = $secretKey;
+    }
+
+    public function request(array $params)
+    {
+        $params['Format'] = 'JSON';
+        $params['Version'] = $this->version;
+        $params['AccessKeyId'] = $this->secretId;
+        $params['SignatureMethod'] = 'HMAC-SHA1';
+        $params['Timestamp'] = gmdate($this->dateTimeFormat);
+        $params['SignatureVersion'] = '1.0';
+        $params['SignatureNonce'] = uniqid();
+        $plainText = $this->makeSignPlainText($params);
+        //签名
+        $plainText = 'GET&%2F&' . $this->percentencode(substr($plainText, 1));
+        $params['Signature'] = base64_encode(hash_hmac('sha1', $plainText, $this->secretKey . "&", true));
+        $client = new \GuzzleHttp\Client([
+            'verify' => false,
+            'http_errors' => false,
+            'connect_timeout' => 3,
+            'read_timeout' => 10,
+            'debug' => true,
+        ]);
+        $requestUrl = 'http://live.aliyuncs.com/?';
+        foreach ($params as $apiParamKey => $apiParamValue) {
+            $requestUrl .= "$apiParamKey=" . urlencode($apiParamValue) . "&";
+        }
+        $requestUrl = substr($requestUrl, 0, -1);
+        $request = new \GuzzleHttp\Psr7\Request('GET', $requestUrl, [
+            'client' => 'php/1.0.0',
+        ]);
+        $response = $client->send($request);
+        return $response->getBody()->getContents();
+    }
+
+    /**
+     * @param $parameters
+     * @return mixed
+     */
+    private function makeSignPlainText($parameters)
+    {
+        ksort($parameters);
+        $canonicalizedQueryString = '';
+        foreach ($parameters as $key => $value) {
+            $canonicalizedQueryString .= '&' . $this->percentEncode($key) . '=' . $this->percentEncode($value);
+        }
+        return $canonicalizedQueryString;
+    }
+
+    protected function percentEncode($str)
+    {
+        $res = urlencode($str);
+        $res = preg_replace('/\+/', '%20', $res);
+        $res = preg_replace('/\*/', '%2A', $res);
+        $res = preg_replace('/%7E/', '~', $res);
+        return $res;
+    }
+
 }
