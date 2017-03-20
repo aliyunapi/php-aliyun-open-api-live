73d24fa2d91176e3dde59ba59385623df7dd6b0b
xuchao <85825770@qq.com>
2017-03-20 11:16:04 +0800

    init

---
+++ tests/TestCase.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+namespace aliyun\test;
+
+use aliyun\core\Client;
+
+use aliyun\core\profile\DefaultProfile;
+
+class TestCase extends \PHPUnit_Framework_TestCase
+{
+
+    public $client = null;
+
+    function setUp()
+    {
+        $iClientProfile = DefaultProfile::getProfile("cn-hangzhou", "5slyhuy4sv30bmppvgew0rps", "NGYL1I7hXC6SgSqkcE5DJdPgJM8=");
+        $this->client = new Client($iClientProfile);
+    }
+
+
+    function getProperty($propertyKey)
+    {
+        $accessKey = "";
+        $accessSecret = "";
+        $iClientProfile = DefaultProfile::getProfile("cn-hangzhou", "5slyhuy4sv30bmppvgew0rps", "NGYL1I7hXC6SgSqkcE5DJdPgJM8=");
+    }
+
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+use aliyun\core\regions\Endpoint;
+use aliyun\core\regions\EndpointConfig;
+use aliyun\core\regions\EndpointProvider;
+
+// ensure we get report on all possible php errors
+error_reporting(-1);
+// require composer autoloader if available
+$composerAutoload = __DIR__ . '/../vendor/autoload.php';
+if (is_file($composerAutoload)) {
+    require_once($composerAutoload);
+}
+
+/**
+ * 配置网关
+ */
+$endpoint = new Endpoint('cn-hangzhou', EndpointConfig::getRegionIds(), EndpointConfig::getProductDomains());
+EndpointProvider::setEndpoints([ $endpoint ]);
+
+require_once(__DIR__ . '/TestCase.php');
