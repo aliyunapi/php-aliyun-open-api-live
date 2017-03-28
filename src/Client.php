7f67c80d5e37e0b66caf180793c3565dda593c6e
xuchao <85825770@qq.com>
2017-03-28 18:45:43 +0800

    更新

--- src/Client.php
+++ src/Client.php
@@ -21,8 +21,14 @@
 
 namespace aliyun\live;
 
-use aliyun\live\auth\ShaHmac1Signer;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Client as HttpClient;
+use aliyun\guzzle\subscriber\Rpc;
 
+/**
+ * Class Client
+ * @package aliyun\live
+ */
 class Client
 {
     /**
@@ -35,17 +41,12 @@ class Client
      */
     public $accessSecret;
 
-    /**
-     * @var \aliyun\core\auth\SignerInterface 签名算法实例
-     */
-    public $signer;
-
-
+    public $baseUri = 'http://live.aliyuncs.com/';
 
     /**
-     * @var \GuzzleHttp\Client
+     * @var HttpClient
      */
-    public $_httpClient;
+    private $_httpClient;
 
     /**
      * Request constructor.
@@ -56,21 +57,25 @@ class Client
         foreach ($config as $name => $value) {
             $this->{$name} = $value;
         }
-        $this->init();
-    }
-
-    public function init(){
-        $this->signer = new ShaHmac1Signer();
     }
 
     /**
      * 获取Http Client
-     * @return \GuzzleHttp\Client
+     * @return HttpClient
      */
     public function getHttpClient()
     {
         if (!is_object($this->_httpClient)) {
-            $this->_httpClient = new \GuzzleHttp\Client([
+            $stack = HandlerStack::create();
+            $middleware = new Rpc([
+                'accessKeyId' => $this->accessKeyId,
+                'accessSecret' => $this->accessSecret,
+            ]);
+            $stack->push($middleware);
+
+            $this->_httpClient = new HttpClient([
+                'base_uri' => $this->baseUri,
+                'handler' => $stack,
                 'verify' => false,
                 'http_errors' => false,
                 'connect_timeout' => 3,
@@ -81,80 +86,12 @@ class Client
         return $this->_httpClient;
     }
 
-
     /**
      * @param array $params
      * @return string
      */
     public function createRequest(array $params)
     {
-        $params['Format'] = 'JSON';
-        $params['Version'] = '2016-11-01';
-        $params['AccessKeyId'] = $this->accessKeyId;
-        $params['SignatureMethod'] = $this->signer->getSignatureMethod();
-        $params['Timestamp'] = gmdate('Y-m-d\TH:i:s\Z');
-        $params['SignatureVersion'] = $this->signer->getSignatureVersion();
-        $params['SignatureNonce'] = uniqid();
-        //签名
-        $params['Signature'] = $this->computeSignature($params);
-        $requestUrl = $this->composeUrl('http://live.aliyuncs.com/', $params);
-        $response = $this->sendRequest('GET', $requestUrl);
-        return $response->getBody()->getContents();
-    }
-
-    /**
-     * Sends HTTP request.
-     * @param string $method request type.
-     * @param string $url request URL.
-     * @param array $options request params.
-     * @return object response.
-     */
-    public function sendRequest($method, $url, array $options = [])
-    {
-        $response = $request = $this->getHttpClient()->request($method, $url, $options);
-        return $response;
-    }
-
-    /**
-     * 合并基础URL和参数
-     * @param string $url base URL.
-     * @param array $params GET params.
-     * @return string composed URL.
-     */
-    protected function composeUrl($url, array $params = [])
-    {
-        if (strpos($url, '?') === false) {
-            $url .= '?';
-        } else {
-            $url .= '&';
-        }
-        $url .= http_build_query($params, '', '&', PHP_QUERY_RFC3986);
-        return $url;
-    }
-
-    /**
-     * @param array $parameters
-     * @return string
-     */
-    private function computeSignature($parameters)
-    {
-        ksort($parameters);
-        $canonicalizedQueryString = '';
-        foreach ($parameters as $key => $value) {
-            $canonicalizedQueryString .= '&' . $this->percentEncode($key) . '=' . $this->percentEncode($value);
-        }
-        $stringToSign = 'GET&%2F&' . $this->percentencode(substr($canonicalizedQueryString, 1));
-        $signature = $this->signer->signString($stringToSign, $this->accessSecret . "&");
-
-        return $signature;
-    }
-
-    protected function percentEncode($str)
-    {
-        $res = urlencode($str);
-        $res = preg_replace('/\+/', '%20', $res);
-        $res = preg_replace('/\*/', '%2A', $res);
-        $res = preg_replace('/%7E/', '~', $res);
-        return $res;
+        return $this->getHttpClient()->get('/',['query'=>$params]);
     }
 }
