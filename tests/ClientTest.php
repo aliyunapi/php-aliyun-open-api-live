6012021ca9181a69e036cc2eabffaa7a151a0604
xuchao <85825770@qq.com>
2017-04-19 09:32:58 +0800

    单元测试

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -13,21 +13,8 @@ class ClientTest extends TestCase
 
     public function testExpirationTime()
     {
-        $params = [
-            'action' => 'publish',
-            'ip' => '127.0.0.1',
-            'id' => '123456',
-            'app' => 'live.cctv.com',
-            'appname' => 'cctv5',
-            'time' => 1488966279,
-            'usrargs' => 'vhost%3Dlive.opencoding.tv%26auth_key%3D1489569710-0-0-499df36a0e5fd6d2bdbd72b877b7896a',
-            'node' => 'eu6',
-        ];
-
         $this->client->setExpirationTime(1488966279);
         $this->assertEquals(1488966279, $this->client->getExpirationTime());
-
-
     }
 
     public function testSign()
